83200367c9aeacc4d6b5c7f3ebea550768359677
jeetenderbatra <jeetenderbatra@gmail.com>
2018-11-08 20:38:30 +0530

    This is for stashed changes

--- src/main/java/com/demo/git/HelloWoldMain.java
+++ src/main/java/com/demo/git/HelloWoldMain.java
@@ -5,6 +5,7 @@ public class HelloWoldMain {
 	public static void main(String[] args) {
 		System.out.println("Hello World..!");
 		System.out.println("This is enhanced hello world");
+		System.out.println("This is more updated");
 	}
 
 }
